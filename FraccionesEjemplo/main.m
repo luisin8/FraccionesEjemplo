088b0546237f6162ab521ab025baf87e41a173dd
IOS 5 <ios-5@SIN-IMAC08.local>
2012-11-21 15:33:30 -0700

    Initial Commit

---
+++ FraccionesEjemplo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FraccionesEjemplo
+//
+//  Created by IOS 5 on 21/11/12.
+//  Copyright (c) 2012 IOS 5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
